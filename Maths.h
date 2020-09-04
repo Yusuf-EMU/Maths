0a53f7e06c0cf0665838f109c12aa990b9f5e1d0
Yusuf-EMU <56773648+Yusuf-EMU@users.noreply.github.com>
2020-09-03 21:38:36 -0700

    optimizations

--- Maths.h
+++ Maths.h
@@ -42,9 +42,23 @@ float Cotangent(float input) {
 }
 
 float Secant(float input) {
-    return 1/Cosine(input);
+    float returnval;
+    float sum=1, t=1;
+    for(int i=1;i<=80;i++)
+    {
+        t=t*(-1)*input*input/(2*i*(2*i-1));
+        sum=sum+t;
+    }
+    return 1/sum;
 }
 
 float Cosecant(float input) {
-    return 1/Sine(input);
+    float returnval;
+    float sum=1, t=1;
+    for(int i=1;i<=80;i++)
+    {
+        t=t*(-1)*input*input/(2*i*(2*i+1));
+        sum=sum+t;
+    }
+    return 1/sum;
 }
